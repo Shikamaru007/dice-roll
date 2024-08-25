7f5923f482de305b4decc7aef6f117d8e3bf6feb
froZin <bolutolu12@gmail.com>
2024-08-25 12:28:49 +0100

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function rollDice() {
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
 
+    for(i=0; i < numOfDice; i++) {
+        const value = Math.floor(Math.random()*6)+1;
+        values.push(value);
+        images.push(`<img src=dice/${value}.png alt=dice${value}>`)
+    }
+
+    diceResult.textContent = `Dice rolled: ${values.join(", ")}`;
+    diceImages.innerHTML = images.join("");    
+}
